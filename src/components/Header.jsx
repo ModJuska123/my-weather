469cf0165483d6c2226a8aa2721b11a16c116f62
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2024-02-11 21:00:14 +0200

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,22 @@ import './Header.css';
 
 export function Header() {
     return (
-        <header class="main-header">
-        <div class="logo">Orų prognozė</div>
-        
-        <form action="/action_page.php" className="date-set">
-            <label for="birthday">Nuo kurios dienos:</label>
-            <input type="date" id="diena" name="diena"/>
-            <input type="submit" value="Submit"/>
-        </form>
-
-        <button id="add_task" class="btn">Pridėti</button>
-    </header>
+        <header className="main-header">
+            <div className="logo">Orų prognozė</div>
+            <div className="weather_data">
+                <form action="/action_page.php" className="date-set">
+                    <label for="data">Pasirinkti dieną: </label>
+                    <input type="date" id="weatherDate" name="weatherDate" />
+                    <button class="button3">Įvesti</button>
+                </form>
+            </div>
+            <div>
+                <div class="topnav">
+                    <a href="#temperature">Temperatūra</a>
+                    <a href="#humidity">Drėgmė</a>
+                    <a href="#vindSpeed">Vėjo greitis</a>
+                </div>
+            </div>
+        </header>
     );
 }
